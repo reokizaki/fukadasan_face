bc2a577c33219153c33b7d47d04449fbed8cd1b9
kizakireo <kizakey18@gmail.com>
2025-04-22 17:33:24 +0900

    third commit

--- app/model.py
+++ app/model.py
@@ -1,14 +1,21 @@
 # app/model.py
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
 import numpy as np
 
-# 特徴抽出モデル（VGG16のFC1層まで使う）
-base_model = VGG16(weights='imagenet')
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+# モデルのロードを遅延ロードするためのグローバル変数
+model = None
+
+def load_model():
+    global model
+    if model is None:
+        base_model = MobileNetV2(weights='imagenet')
+        model = Model(inputs=base_model.input, outputs=base_model.get_layer('global_average_pooling2d').output)
+    return model
 
 def extract_features(img_path):
+    model = load_model()  # モデルを遅延ロード
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
